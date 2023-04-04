dd12ee6ad46576c6a01b14c2e6882ed07c4e111d
Sergio Hernandez <serhern831@gmail.com>
2023-04-04 19:26:01 +0000

    stateless deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@o71s+_^+tm0*33j7gv3xrjjtue5)z55sjg40urg(7pu*8kl)5'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [ "*" ]
-
+CSRF_TRUSTED_ORIGINS = [ "https://*.run.app" ]
 
 # Application definition
 
